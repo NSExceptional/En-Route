1183303f99ad63ca7780aadd9a85bf2c6b1dd77b
ThePantsThief <tannerbennett@me.com>
2016-01-15 19:35:07 -0600

    User location tracking working

--- En Route/ERAppDelegate.m
+++ En Route/ERAppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.tintColor = [UIColor colorWithRed:0.973 green:0.271 blue:0.298 alpha:1.000];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[ERMapViewController new]];
     
     [self.window makeKeyAndVisible];
--- En Route/ERMapViewController.m
+++ En Route/ERMapViewController.m
@@ -8,6 +8,12 @@
 
 #import "ERMapViewController.h"
 
+@interface ERMapViewController () <CLLocationManagerDelegate>
+
+@property (nonatomic) CLLocationManager *locationManager;
+
+@end
+
 
 @implementation ERMapViewController
 
@@ -23,15 +29,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // Locaiton manager is used to get location permissions
+    self.locationManager = [CLLocationManager new];
+    self.locationManager.delegate = self;
+    
     UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithTitle:@"Clear" style:UIBarButtonItemStylePlain target:self action:@selector(clearButtonPressed)]; //targets self bc clear button
     self.navigationItem.leftBarButtonItem = button; //clear button
     
     self.navigationController.toolbarHidden = NO;
     self.title = @"En Route";
+    
+    MKUserTrackingBarButtonItem *userTrackingButton = [[MKUserTrackingBarButtonItem alloc] initWithMapView:self.mapView];
+    self.toolbarItems = @[userTrackingButton];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self.locationManager requestWhenInUseAuthorization];
 }
 
 - (void)clearButtonPressed {
     
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    // User granted location access
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        [self.mapView setUserTrackingMode:MKUserTrackingModeFollow animated:YES];
+    } else {
+        // User denied location access
+    }
 }
 
 @end
